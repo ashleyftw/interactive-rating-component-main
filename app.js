675464b8503976fa23be7e3425dc8795dde81ff4
Ashley Wang <127802589+ashleyftw@users.noreply.github.com>
2023-04-18 00:11:42 -0500

    add js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const ratings = document.querySelector('.ratings-btn');
+const ratings = document.querySelector('.rating-btn button');
+const ratingCard = document.querySelector('.rating-card');
+const resultsCard = document.querySelector('.results-card');
+const rates = document.querySelectorAll('.ratings-btn');
+const rating = document.querySelector('.rating');
+
 ratings.addEventListener('click', () => {
-    alert('you clicked me!')
+    resultsCard.classList.remove('hidden');
+    ratingCard.classList.add('hidden')
  })
  
+ rates.forEach((rate) => {
+    rate. addEventListener('click', ()=> {
+        const rateNum = rate.innerHTML;
+        rating.innerText = rateNum;
+     })
+ })
